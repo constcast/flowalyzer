6721636ea03eec7f555296fc72cd1c78208ffade
Lothar Braun <braun@net.in.tum.de>
2011-11-15 10:39:39 +0100

    Print usage message on wrong config elements

--- flowalyzer.py
+++ flowalyzer.py
@@ -12,7 +12,7 @@ import sys, yaml
 if __name__ == "__main__":
 	parser = OptionParser("usage: %prog [options]")
 	parser.add_option('-c', '--config', dest="configfile",
-			  help = "config file in yml format")
+			  help = "config file in yml format (required)")
 			  
 	(options, args) = parser.parse_args()
 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 			print "Could not open config file \"%s\": %s" % (options.configfile, e)
 			sys.exit(-1)
 	else:
-		print "You need to specify a yml config file. We do not yet support command line configuration, yet :("
+		parser.print_help()
 		sys.exit(-1)
 
 	main = MainModule(config)
