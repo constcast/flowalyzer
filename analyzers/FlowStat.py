e5bbf5f588687eb0bdd2ab2131cd2216a2d3840e
Lothar Braun <braun@net.in.tum.de>
2011-11-16 16:47:27 +0100

    Generate proper images

--- analyzers/FlowStat.py
+++ analyzers/FlowStat.py
@@ -34,7 +34,6 @@ class RRDGenerator:
 				os.system(command)
 				self.counter[i] = 0
 				self.intervalStart[i] = currTime
-				self.createImage('imgs/', currTime)
 
 	def update(self, flow_record):
 		for i in range(0, len(self.intervals)):
@@ -44,13 +43,13 @@ class RRDGenerator:
 				self.intervalStart[i] = flow_record[8]
 		self.checkRRD(flow_record[8])
 
-	def createImage(self, imgDir, endTime):
+	def createImages(self, imgDir, endTime):
 		pngImg = imgDir + '/%s-%d.png'
 		for i in range(0, len(self.intervals)):
 			rrdName = self.rrdDBName % (self.reportname, i)
-			command = "rrdtool graph %s -imgformat PNG --end %d --start %d-%dm DEF:ds0b=%s:s:MAX LINE1:ds0b#9999FF:\"min/max\" DEF:ds0c=%s:s:MIN LINE1:ds0c#9999FF DEF:ds0a=%s:s:AVERAGE LINE2:ds0a#0000FF:\"average\"" % (pngImg % (self.reportname, self.intervals[i]), endTime, endTime, self.graphHistory[i], rrdName, rrdName, rrdName)
+			command = "rrdtool graph %s --width=800 --height=600 --end %d --start %d-%dm DEF:ds0b=%s:input:MAX LINE1:ds0b#9999FF:\"min/max\" DEF:ds0c=%s:input:MIN LINE1:ds0c#9999FF DEF:ds0a=%s:input:AVERAGE LINE2:ds0a#0000FF:\"average\"" % (pngImg % (self.reportname, self.intervals[i]), endTime, endTime, self.graphHistory[i], rrdName, rrdName, rrdName)
 			os.system(command)
-			print command
+			#print command
 
 class Analyzer(BaseAnalyzer):
 	def __init__(self, config):
@@ -62,7 +61,7 @@ class Analyzer(BaseAnalyzer):
 		if not os.access(self.imgDir, os.R_OK | os.W_OK):
 			os.mkdir(self.imgDir)
 
-		rrdintervals = (2, 12, 720)
+		rrdintervals = (60, 180, 720)
 		rrdgraphhist = (120, 1440, 43200)
 
 		self.packetReport = RRDGenerator("Packets", self.rrdDir, rrdintervals, rrdgraphhist, 7)
@@ -72,4 +71,7 @@ class Analyzer(BaseAnalyzer):
 		for flow in flows:
 			self.packetReport.update(flow)
 			self.bytesReport.update(flow)
+		self.packetReport.createImages(self.imgDir, flows[-1][8])
+		self.bytesReport.createImages(self.imgDir, flows[-1][8])
+ 
 		
