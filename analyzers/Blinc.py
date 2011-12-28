61bf3a2c2bb0c1d1891421e0e7a4e963367e49a2
Lothar Braun <braun@net.in.tum.de>
2011-12-28 17:12:49 +0100

    Started blinc module

---
+++ analyzers/Blinc.py
@@ -0,0 +1,28 @@
+"""
+Reassembles the classifcation algorithm presented in "BLINC: Multilevel Traffic Classification in the Dark"
+"""
+
+class Host:
+	def __init__(self):
+		pass
+
+	def update(self, flow):
+		pass
+
+from BaseAnalyzer import BaseAnalyzer
+
+class Analyzer(BaseAnalyzer):
+	def __init__(self, config, reportIntervals, workingdir):
+		BaseAnalyzer.__init__(self, config, reportIntervals, workingdir)
+
+		self.hostmap = dict()
+	
+	
+	def processFlow(self, flow):
+		pass
+		
+	def generateReport(self, reportNumber, reportTime):
+		pass
+		
+		
+
