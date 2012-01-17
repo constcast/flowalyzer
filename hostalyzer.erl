a6868781e38ee7271ac71da866cfbd4e8483156e
Lothar Braun <braun@net.in.tum.de>
2012-01-17 15:33:37 +0100

    Update host stats

--- hostalyzer.erl
+++ hostalyzer.erl
@@ -1,12 +1,43 @@
 -module(hostalyzer).
 -export([start/0]).
 
--record(state, {hostMap}).
--record(host, {ip, srcPorts, dstPorts, dstIPs}).
+-include("flows.hrl").
 
-processFlow(Flow, State) ->
-    
-    State.
+-record(state, {hostMap}).
+-record(host, {ip, 
+	       flowCount = 0, 
+	       srcPorts = dict:new(), 
+	       dstPorts = dict:new(), 
+	       dstIPs = dict:new()}).
+
+%%% Creates a new host record for storing the statistics on the
+%%% given host
+createHost(IP) ->
+    #host{ip=IP}.
+
+updateHostInfo(Host, Flow) ->
+    FlowCount = Host#host.flowCount + 1,
+    % for all dstPorts, srcPorts and dstIps update all statistics
+    UpdateDict = fun(Dict, Value) -> dict:update_counter(Value, 1, Dict) end,
+    % return updated hostinformation
+    Host#host{flowCount = FlowCount,
+	     srcPorts = UpdateDict(Host#host.srcPorts, Flow#flow.srcPort),
+	     dstPorts = UpdateDict(Host#host.dstPorts, Flow#flow.dstPort),
+	     dstIPs   = UpdateDict(Host#host.dstIPs,   Flow#flow.dstIP)}.
+
+processFlow(Flow, State) ->    
+    % update the host map with new values
+    case dict:is_key(Flow#flow.srcIP, State#state.hostMap) of
+       true ->
+	    Update = fun(X) ->  updateHostInfo(X, Flow) end,
+	    NewHostMap = dict:update(Flow#flow.srcIP, Update, State#state.hostMap);
+       false ->
+	    Host = updateHostInfo(createHost(Flow#flow.srcIP), Flow),
+	    NewHostMap = dict:store(Flow#flow.srcIP, Host, State#state.hostMap)
+    end,
+				     
+    % return updated state
+    State#state{hostMap = NewHostMap}.
 
 start() ->
     State = #state{hostMap = dict:new()},
