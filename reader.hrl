d07ed6625ec47f54c6a4e835b9e2871fe03580fc
Lothar Braun <braun@net.in.tum.de>
2012-01-18 10:20:24 +0100

    Updated record documentation. Introduced new fields

--- reader.hrl
+++ reader.hrl
@@ -1,7 +1,10 @@
--record(readerData, {handle, 
-		    flowDest,
-		    startTime = 0,
-		    firstEntry = 0 , 
-		    lastEntry = 0, 
-		    currTime = 0, 
-		    winSize = 0}).
+-record(readerData, {handle,       % DB handle
+		    flowDest,      % Process id of flow sink
+		    startTime = 0, % Timestamp of process stat
+		    firstEntry = 0,% Timestamp of the first flow in the DB
+		    lastEntry = 0, % Timestamp of the last flow in the DB (at startup time)
+		    firstQuery = 0,% Get flows with firstSwiched >= firstQuery
+		    lastQuery  = 0,% Get flows with firstSwitched <= lastQuery
+		    currTime = 0,  % Current time stamp in flow table (points to the flow firstswitchedTime of the last send flow)
+		    winSize = 0    % Window size for flows to get as chunk from the DB
+		    }).
